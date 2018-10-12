51c12f9e2931589aab9c9117e19bc9b965c7909f
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2018-10-12 15:31:05 +0900

    MenuSystem Update

---
+++ Assets/Script/Menu/MenuManeger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManeger : MonoBehaviour {
+
+    public void OnEnterContinueGame()
+    {
+        Debug.Log("ゲーム画面へ戻る");
+        SceneManager.UnloadSceneAsync("SystemMenu");
+    }
+
+    public void LoadStageSelect()
+    {
+        SceneManager.LoadSceneAsync("StageSelect");
+    }
+
+    public void ApplicationExit()
+    {
+        Application.Quit();
+    }
+}
